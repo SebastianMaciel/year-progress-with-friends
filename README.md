409ed7a566660c3231748e53852dbb42546c272e
Sebastián <sebastianmaciel@outlook.com>
2020-11-21 16:38:37 -0300

    Parece que teminé nomás

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# desafioTresJuaniGallo
- Amar un “progreso del año” en donde muestre en una barra el % de completitud del mismo. La idea es que esta barra sea dinámica.
+# Desafío Nº3 - Juani Gallo
+
+Desafío: Armar un “progreso del año” en donde muestre en una barra el % de completitud del mismo. La idea es que esta barra sea dinámica.
+
+
+**Miralo en vivo en Netlify: [Desafío 3](https://desafio-tres-juani-gallo.netlify.app/)**
+
+---
+
+## Qué se usa
+
+
+Acá usamos **MomentJS** para calcular las fechas.
+
+Un poco de HTML, CSS un poco más y JS.
+
+Usamos jQuery por costumbre nomás.
+
+
+## Dificultad/Valoración
+
+7 Sebas de Oro
+
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/9bvBx6H/sebadeoro-mini.png)
+![](https://i.ibb.co/k8wPsDr/sebadeoro-mini-disabled.png)
+![](https://i.ibb.co/k8wPsDr/sebadeoro-mini-disabled.png)
+![](https://i.ibb.co/k8wPsDr/sebadeoro-mini-disabled.png)
+
+
+## Algo para remarcar?
+
+Me volví medio loco con el tema de sacar los minutos restantes, y setear la barra de progreso un toque, pero porque quería que el programa sea legible para humanos. Nada más.
+
+---
+
+Este es el desafío propuesto en el twitter de [@JuaniGallo](https://twitter.com/JuaniGallo)
---
+++ index.js
@@ -0,0 +1,33 @@
+// MomentJS: calculamos el día de hoy y seteamos el día de fin de año
+let calcularHoy = moment(moment().format('YYYY-MM-DD')),
+    hastaFinDeAnio = moment(moment().format('2020-12-31'));
+
+// Con esta función calculamos para el porcentaje de la barra y el texto de adentro
+let porcentajeParaFinDeAnio = () => {
+  let diasParaFinDeAnio = Number(calcularHoy.diff(hastaFinDeAnio, 'days')), // Esto saca en días, lo que resta para fin de año
+      diasDelAnio = 365,                                                    // Seteamos los días del año
+      diasQueFaltan = diasDelAnio - Math.abs(diasParaFinDeAnio),            // Los días del año restando los días que faltan
+
+      // Calculamos el porcentaje restante para el progress
+      porcentajeRestante = (diasQueFaltan * 100) / diasDelAnio;
+      
+      // Seteamos la barra de progreso
+      $('#barraDeProgreso').attr('style', `width: ${porcentajeRestante}%`)
+      // Ponemos el número para el usuario
+      $('#porcentajeRestante').text(`${porcentajeRestante.toFixed()}%`)
+}
+
+// Esto es lo importante, cuánto vamos a tener que ver de Friends para no aburrirnos...
+let capitulosDeFriendsParaFinDeAnio = () => {
+  let minutosParaFinDeAnio = Number(calcularHoy.diff(hastaFinDeAnio, 'minutes')), // Calculamos los minutos que faltan para fin de año
+      minutosPorCapitulo = 22                                                     // Promedio de duración de capítulos
+      capitulosParaFinDeAnio = minutosParaFinDeAnio / minutosPorCapitulo,         // Calculamos cuánto vamos a poder que mirar
+
+      // Seteamos la info imprescindible para el usuario...
+      $('#capitulosParaFinDeAnio').text(Math.abs(capitulosParaFinDeAnio.toFixed()))
+}
+
+// Ejecutamos la función
+porcentajeParaFinDeAnio()
+// Y la más importante realmente
+capitulosDeFriendsParaFinDeAnio()
