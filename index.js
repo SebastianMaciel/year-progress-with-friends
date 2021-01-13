0862be237c19708a46f688fd5045a59ce79f298b
Sebastián Maciel <sebastianmaciel@outlook.com>
2021-01-13 19:21:31 -0300

    Correción visual

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 // MomentJS: calculamos el día de hoy y seteamos el día de fin de año
-let calcularHoy = moment(moment().format('YYYY-MM-DD')),
-    hastaFinDeAnio = moment(moment().format('2021-12-31'));
+let calcularHoy = moment(moment().format("YYYY-MM-DD")),
+  hastaFinDeAnio = moment(moment().format("2021-12-31"));
 
 // Con esta función calculamos para el porcentaje de la barra y el texto de adentro
 let porcentajeParaFinDeAnio = () => {
-  let diasParaFinDeAnio = Number(calcularHoy.diff(hastaFinDeAnio, 'days')), // Esto saca en días, lo que resta para fin de año
-      diasDelAnio = 365,                                                    // Seteamos los días del año
-      diasQueFaltan = diasDelAnio - Math.abs(diasParaFinDeAnio),            // Los días del año restando los días que faltan
+  let diasParaFinDeAnio = Number(calcularHoy.diff(hastaFinDeAnio, "days")), // Esto saca en días, lo que resta para fin de año
+    diasDelAnio = 365, // Seteamos los días del año
+    diasQueFaltan = diasDelAnio - Math.abs(diasParaFinDeAnio), // Los días del año restando los días que faltan
+    // Calculamos el porcentaje restante para el progress
+    porcentajeRestante = (diasQueFaltan * 100) / diasDelAnio;
 
-      // Calculamos el porcentaje restante para el progress
-      porcentajeRestante = (diasQueFaltan * 100) / diasDelAnio;
-      
-      // Seteamos la barra de progreso
-      $('#barraDeProgreso').attr('style', `width: ${porcentajeRestante}%`)
-      // Ponemos el número para el usuario
-      $('#porcentajeRestante').text(`${porcentajeRestante.toFixed()}%`)
-}
+  // Seteamos la barra de progreso
+  $("#barraDeProgreso").attr("style", `width: ${porcentajeRestante}%`);
+  // Ponemos el número para el usuario
+  $("#porcentajeRestante").text(`${porcentajeRestante.toFixed()}%`);
+};
 
 // Esto es lo importante, cuánto vamos a tener que ver de Friends para no aburrirnos...
 let capitulosDeFriendsParaFinDeAnio = () => {
+  let calcularHoy = moment(moment().format("YYYY-MM-DD"));
 
-  let calcularHoy = moment(moment().format('YYYY-MM-DD'))
-
-  let minutosParaFinDeAnio = Number(calcularHoy.diff(hastaFinDeAnio, 'minutes')), // Calculamos los minutos que faltan para fin de año
-      minutosPorCapitulo = 22                                                     // Promedio de duración de capítulos
-      capitulosParaFinDeAnio = minutosParaFinDeAnio / minutosPorCapitulo,         // Calculamos cuánto vamos a poder mirar
-
-      // Seteamos la info imprescindible para el usuario...
-      $('#capitulosParaFinDeAnio').text(Math.abs(capitulosParaFinDeAnio.toFixed()))
-}
+  let minutosParaFinDeAnio = Number(calcularHoy.diff(hastaFinDeAnio, "minutes")), // Calculamos los minutos que faltan para fin de año
+    minutosPorCapitulo = 22; // Promedio de duración de capítulos
+  (capitulosParaFinDeAnio = minutosParaFinDeAnio / minutosPorCapitulo), // Calculamos cuánto vamos a poder mirar
+    // Seteamos la info imprescindible para el usuario...
+    $("#capitulosParaFinDeAnio").text(Math.abs(capitulosParaFinDeAnio.toFixed()));
+};
 
 // Ejecutamos la función
-porcentajeParaFinDeAnio()
+porcentajeParaFinDeAnio();
 // Y la más importante realmente
-capitulosDeFriendsParaFinDeAnio()
+capitulosDeFriendsParaFinDeAnio();
